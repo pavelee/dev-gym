4e45e806d6c5e1aaeddf0249bf4a06416ea02d41
Paweł Ciosek <pciosek@Mac-mini-Pawe.local>
2023-07-20 00:23:06 +0200

    security fixes

--- pages/login.tsx
+++ pages/login.tsx
@@ -7,8 +7,8 @@ const Login: ExtendedNextPage = () => {
             type="login"
             formProps={{
                 initialValues: {
-                    email: "user@example.com",
-                    password: "1234",
+                    email: "",
+                    password: "",
                 },
             }}
         />
